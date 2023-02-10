2f39fd7c705a6d51f071b12bb96e02a35b3ec9ca
Dmitry Pavlovich <dimonza49@gmail.com>
2023-02-10 04:02:20 +0300

    add Cards struct and some logic

--- Card.go
+++ Card.go
@@ -10,7 +10,7 @@ var names = [13]string{"A", "K", "Q", "V", "10", "9", "8", "7", "6", "5", "4", "
 var suits = [4]string{"spade", "diamond", "club", "heart"}
 var values = [13]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
 
-func CreateDeck32() []Card {
+func CreateDeck36() []Card {
 	var cards []Card
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 9; j++ {
